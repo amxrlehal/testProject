810b2cd101fe213e2c3c3689e286e0ed04524612
Amar <amarklehal@icloud.com>
2022-08-18 15:10:39 +0100

    i think this will work?

---
+++ testProject/AssessmentViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AssessmentViewController.swift
+//  testProject
+//
+//  Created by Amar Lehal on 18/08/2022.
+//
+
+import UIKit
+
+class AssessmentViewController: UIViewController {
+    
+    
+    @IBOutlet weak var recText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+        
+        
+        
+    }
+    
+    @IBAction func isoSlider(_ sender: UISlider) {
+        let sliderValue = sender.value
+        if sliderValue > 50 {
+        
+            
+            
+        }
+        
+       
+        
+        
+    }
+    
+    
+
+}
